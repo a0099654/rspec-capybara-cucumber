4740ab13614dbaf2b38c26c9b017e95e08c18840
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-21 11:33:54 +0800

    [Stella]Create assertion to test mood in the playground

--- spec/playground_spec.rb
+++ spec/playground_spec.rb
@@ -2,7 +2,11 @@ require_relative '../lib/playground'
 
 describe Playground do
     context 'when there are no children' do
-        it 'is quite boring place'
+        
+        it 'is quite boring place' do
+            expect(mood).to eq('boring')
+        end
+        
         it 'is empty' 
     end
 end
